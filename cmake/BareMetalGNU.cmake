a32a86bdce4e34768e0daff5e5e0151d5f4ac397
takumin <takumiiinn@gmail.com>
2016-02-24 01:48:47 +0900

    fix

--- cmake/BareMetalGNU.cmake
+++ cmake/BareMetalGNU.cmake
@@ -16,7 +16,8 @@ else()
   message(FATAL_ERROR "Please set to \${CMAKE_SYSTEM_PROCESSOR} variable.")
 endif()
 
-set(LINKER_FLAGS "-Wl,--start-group -lc -lg -lm -lstdc++ -lsupc++ -Wl,--end-group -Wl,--gc-sections -Wl,-Map=sections.map")
+set(LINKER_FLAGS "-Wl,--start-group -lc -lg -lgcc -lm -lstdc++ -lsupc++ -Wl,--end-group")
+set(LINKER_FLAGS "${LINKER_FLAGS} -Wl,--gc-sections -Wl,-Map=sections.map")
 
 set(CMAKE_C_FLAGS "${CPU_FLAGS} -std=c11 -Wall -Wextra -pipe -ffunction-sections -fdata-sections")
 set(CMAKE_CXX_FLAGS "${CPU_FLAGS} -std=c++14 -Wall -Wextra -pipe -ffunction-sections -fdata-sections")
@@ -34,6 +35,11 @@ set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS}" CACHE STRING "Modul
 set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS}" CACHE STRING "Shared Library Linker Flags.")
 set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS}" CACHE STRING "Static Library Linker Flags.")
 
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "-O3 -flto" CACHE STRING "Flags used by the linker during release builds.")
+set(CMAKE_MODULE_LINKER_FLAGS_RELEASE "-O3 -flto" CACHE STRING "Flags used by the linker during release builds.")
+set(CMAKE_SHARED_LINKER_FLAGS_RELEASE "-O3 -flto" CACHE STRING "Flags used by the linker during release builds.")
+set(CMAKE_STATIC_LINKER_FLAGS_RELEASE "-O3 -flto" CACHE STRING "Flags used by the linker during release builds.")
+
 if(NOT COMPILER_PREFIX)
   set(COMPILER_PREFIX arm-none-eabi-)
 endif()
@@ -48,7 +54,7 @@ CMAKE_FORCE_C_COMPILER(${CMAKE_C_COMPILER} GNU)
 CMAKE_FORCE_CXX_COMPILER(${CMAKE_CXX_COMPILER} GNU)
 
 execute_process(
-  COMMAND ${CMAKE_C_COMPILER} -print-file-name=libc.a
+  COMMAND ${CMAKE_C_COMPILER} --print-libgcc-file-name
   OUTPUT_VARIABLE CMAKE_INSTALL_PREFIX
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
